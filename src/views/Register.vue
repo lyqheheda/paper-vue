9e0313de690ce5def5a262fb5e08039e2a8692cd
lyqheheda <50129469+lyqheheda@users.noreply.github.com>
2021-04-19 11:15:37 +0800

    Update Register.vue

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,27 +1,36 @@
 <template>
     <div>
-<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-container>
+      <el-header>
+        <img class="mlogo" src="https://www.markerhub.com/dist/images/logo/markerhub-logo.png" alt="">
+      </el-header>
+      <el-main>
+<el-form :model="registerForm" :rules="rules" ref="registerForm" label-width="100px" class="demo-ruleForm">
   
-  <el-form-item label="user type" prop="user_type">
-    <el-select v-model="ruleForm.user_type" placeholder="please select">
-      <el-option label="author" value="author"></el-option>
-      <el-option label="reviewer" value="reviewer"></el-option>
+  <el-form-item label="user type" prop="rname">
+    <el-select v-model="registerForm.rname" placeholder="please select">
+      <!-- C代表作者 -->
+      <el-option label="author" value="C"></el-option> 
+<!-- B代表审稿人 -->
+      <el-option label="reviewer" value="B"></el-option>
     </el-select>
   </el-form-item>
-  <el-form-item label="Real name"  prop="realName" v-if="ruleForm.user_type==='author'">
-    <el-input v-model="ruleForm.realName"></el-input>
+  <el-form-item label="Author id"  prop="aut_id" v-if="registerForm.rname==='C'">
+    <el-input v-model="registerForm.aut_id"></el-input>
   </el-form-item>
-  <el-form-item label="user name" prop="username">
-    <el-input v-model="ruleForm.username"></el-input>
+  <el-form-item label="user name" prop="uname">
+    <el-input v-model="registerForm.uname"></el-input>
   </el-form-item>
   <el-form-item label="password" prop="password">
-    <el-input v-model="ruleForm.password"></el-input>
+    <el-input v-model="registerForm.password"></el-input>
   </el-form-item>
   <el-form-item>
-    <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-    <el-button @click="resetForm('ruleForm')">重置</el-button>
+    <el-button type="primary" @click="submitForm('registerForm')">立即创建</el-button>
+    <el-button @click="resetForm('registerForm')">重置</el-button>
   </el-form-item>
 </el-form>
+</el-main>
+    </el-container>
     </div>
 </template>
 
@@ -29,36 +38,27 @@
   export default {
     data() {
       return {
-        ruleForm: {
-          realName: '',
-          username:'',
+        registerForm: {
+          aut_id: '',
+          uname:'',
           password:'',
-          user_type: '',
+          rname: '', //权限名称，B代表审稿人，C代表作者
           
         },
         rules: {
-          name: [
-            { required: true, message: '请输入活动名称', trigger: 'blur' },
-            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          rname: [
+            { required: true, message: 'Please select', trigger: 'blur' },
           ],
-          user_type: [
-            { required: true, message: '请选择活动区域', trigger: 'change' }
+          aut_id:[
+            { required: true, message: 'Please enter', trigger: 'blur' },
           ],
-          date1: [
-            { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+           uname: [
+           { required: true, message: 'Please enter', trigger: 'blur' },
           ],
-          date2: [
-            { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
+          password: [
+            { required: true, message: 'Please enter', trigger: 'blur' },
           ],
-          type: [
-            { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
-          ],
-          resource: [
-            { required: true, message: '请选择活动资源', trigger: 'change' }
-          ],
-          desc: [
-            { required: true, message: '请填写活动形式', trigger: 'blur' }
-          ]
+          
         }
       };
     },
@@ -67,6 +67,22 @@
         this.$refs[formName].validate((valid) => {
           if (valid) {
             alert('submit!');
+            const _this = this
+            this.$axios.post('/register', this.registerForm).then(res => {
+            //处理一下用户名重复
+            if(res.data.isRegistered){
+              _this.$router.push("/blogs")
+
+            }
+            else {
+              alert(res.data.data)
+            }
+
+
+
+
+
+            })
           } else {
             console.log('error submit!!');
             return false;
@@ -81,5 +97,34 @@
 </script>
 
 <style scoped>
+.el-main {
+    /*background-color: #E9EEF3;*/
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+    body > .el-container {
+    margin-bottom: 40px;
+  }
 
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+
+  .mlogo {
+    height: 60%;
+    margin-top: 10px;
+  }
+
+    .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
 </style>

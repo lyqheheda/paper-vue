b8801e346c9f2ae3247099daaecf7d88911b0ae8
lyqheheda <50129469+lyqheheda@users.noreply.github.com>
2021-04-23 15:29:46 +0800

    Update Reviewer.vue

--- src/views/Reviewer.vue
+++ src/views/Reviewer.vue
@@ -3,35 +3,49 @@
     <Header :showLogin="true"></Header>
 
     <el-container>
-        <el-aside width="200px">
-          <div style="margin-top: 15px;" class='searchbox'>
-    <el-input placeholder="关键词" v-model="skeywords" class="input-with-select">
-      <el-button slot="append" icon="el-icon-search" @click='update' ></el-button>
-    </el-input>
-  </div>
-  
-    <el-checkbox-group v-model="checkList">
-    <el-checkbox label='showPassed' @change='update'>show reviewed</el-checkbox>
-    <el-checkbox label="timeOrder" @change='update'>order by time</el-checkbox>
-  </el-checkbox-group>
- 
-  
-</el-aside>
-      <el-main><div class="block">
-      <el-pagination
-        class="mpage"
-        background
-        layout="prev, pager, next"
-        :current-page="currentPage"
-        :page-size="pageSize"
-        :total="total"
-        @current-change="page"
-      >
-      </el-pagination>
-    </div></el-main>
-    </el-container>
+      <el-aside width="200px">
+        <div style="margin-top: 15px" class="searchbox">
+          <el-input
+            placeholder="关键词"
+            v-model="skeywords"
+            class="input-with-select"
+          >
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+              @click="update"
+            ></el-button>
+          </el-input>
+        </div>
 
-    
+        <el-checkbox-group v-model="checkList">
+          <el-checkbox label="showPassed" @change="update"
+            >show reviewed</el-checkbox
+          >
+          <el-checkbox label="timeOrder" @change="update"
+            >order by time</el-checkbox
+          >
+        </el-checkbox-group>
+      </el-aside>
+      <el-main>
+        <div>
+          
+        </div>
+
+
+        <div class="block">
+          <el-pagination
+            class="mpage"
+            background
+            layout="prev, pager, next"
+            :current-page="currentPage"
+            :page-size="pageSize"
+            :total="total"
+            @current-change="page"
+          >
+          </el-pagination></div
+      ></el-main>
+    </el-container>
   </div>
 </template>
 
@@ -43,30 +57,33 @@ export default {
   components: { Header },
   data() {
     return {
-      checkList: ['showPassed'],
-      skeywords:'',
+      checkList: ["showPassed"],
+      skeywords: "",
       user: {
         username: "",
       },
       hasLogin: false,
 
       // blogs: {},
-      blogs: [
-        {
-          paper_id: 11,
-          userId: 1,
-          pname: "1111",
-          paperAbstract: "111",
-          content: "111",
-          aname: "11",
-          keyword: "11",
-          created: "2020-4-3T10:23:23",
-          status: 0,
-        },
-      ],
+      // blogs: [
+      //   {
+      //     paper_id: 11,
+      //     userId: 1,
+      //     pname: "1111",
+      //     paperAbstract: "111",
+      //     content: "111",
+      //     aname: "11",
+      //     keyword: "11",
+      //     created: "2020-4-3T10:23:23",
+      //     status: 0,
+      //   },
+      // ],
       currentPage: 1,
       total: 0,
       pageSize: 5,
+      paperList:[],
+      rev_id:0,
+
     };
   },
   methods: {
@@ -74,25 +91,35 @@ export default {
       const _this = this;
       _this.$axios.get("/reviewer?currentPage=" + currentPage).then((res) => {
         console.log(res);
-        _this.blogs = res.data.data.records;
+        _this.paperList = res.data.data.records;
         _this.currentPage = res.data.data.current;
         _this.total = res.data.data.total;
         _this.pageSize = res.data.data.size;
       });
     },
-    update(){
-      console.log(this.checkList)
-      console.log(this.skeywords)
+    update() {
+      // console.log(this.checkList)
+      // console.log(this.checkList.includes('timeOrder'))
+      // console.log('=====')
+
+      let params = {
+        rev_id:this.rev_id,
+        currentPage: this.currentPage,
+        numEachPage: this.pageSize,
+        skeywords: this.skeywords,
+        timeOrder: this.checkList.includes("timeOrder"),
+        showPassed: this.checkList.includes("showPassed"),
+      };
       const _this = this;
-      _this.$axios.get()//看一下收藏夹文档
+      _this.$axios.get("/reviewer", params).then((res) => {
 
-
-    }
+      });
+    },
   },
 
   created() {
     this.page(1);
-    if (this.$store.getters.getUser.uname) {
+    if (this.$store.getters.getUser.uname) {//需要初始化rev_id（从userinfo读取）
       this.user.username = this.$store.getters.getUser.uname;
 
       console.log("123123123");
@@ -114,48 +141,48 @@ export default {
   text-align: center;
 }
 
-.el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    /* line-height: 300px; */
-  }
-  
-  .el-main {
-    /* background-color: #989ea3; */
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  }
-  
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-  
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-  
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
 
-  .searchbox{
-    width: 80%;
-    margin: 0 auto;
-    
-  }
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  /* line-height: 300px; */
+}
 
-  el-checkbox{
-    margin-top: 100px;
-    margin-bottom: 100px;
-  }
+.el-main {
+  /* background-color: #989ea3; */
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+
+.searchbox {
+  width: 80%;
+  margin: 0 auto;
+}
+
+el-checkbox {
+  margin-top: 100px;
+  margin-bottom: 100px;
+}
 </style>
